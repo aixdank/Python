3670d3c0bbed0d78d46a57a21849f2a7abedbc16
aixdank <aidank15839@gmail.com>
2025-07-06 20:38:34 +0530

    Create exam_memes.py
    "Added exam meme generator"

---
+++ exam_memes.py
@@ -0,0 +1,14 @@
+# July 2025 Exam Meme Generator
+import random
+
+MEMES = [
+    "When you see the exam paper:\n(ﾟ〇ﾟ;)",
+    "Me trying to remember code:\n(¯―¯٥)",
+    "Snake Year saving my grades:\n〜(꒪꒳꒪)〜",
+    "Open-book exam? More like:\n(╯°□°)╯︵ ┻━┻",
+    "That one student who finished early:\n(ಠ_ಠ)"
+]
+
+print("🎯 TODAY'S EXAM MEME:")
+print(random.choice(MEMES))
+print("\n#ExamSurvival #Python")
